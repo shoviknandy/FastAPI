2d8b4e6230411487f58be85ed1548e84c6831027
shoviknandy <53639158+shoviknandy@users.noreply.github.com>
2024-09-24 13:42:36 +0530

    Delete utilities.py

--- utilities.py
+++
@@ -1,8 +0,0 @@
-from passlib.context import CryptContext
-
-#hashing password
-pwdcontext=CryptContext(schemes=['bcrypt'],deprecated="auto")
-
-def hash(password: str):
-    return pwdcontext.hash(password)
-
